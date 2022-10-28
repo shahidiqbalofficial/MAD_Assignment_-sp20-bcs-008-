66665a9d9a8ddb7def11fc717e279ef80a27fe28
shahid iqbal <109196236+shahidiqbalofficial@users.noreply.github.com>
2022-10-28 23:32:48 +0500

    Add files via upload

---
+++ Theory/Assignment-02 (Login page)/src/Login.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, Text, Touchable, TouchableOpacity} from 'react-native';
+import Background from './Background';
+import Btn from './Btn';
+import {darkGreen} from './Constants';
+import Field from './Field';
+
+const Login = (props) => {
+  return (
+    <Background>
+      <View style={{alignItems: 'center', width: 460}}>
+        <Text
+          style={{
+            color: 'green',
+            fontSize: 40,
+            fontWeight: 'bold',
+            marginVertical: 20,
+          }}>
+          Registeration
+        </Text>
+        <View
+          style={{
+            backgroundColor: 'gray',
+            height: 700,
+            width: 460,
+            borderTopLeftRadius: 130,
+            paddingTop: 100,
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 40, color:'black', fontWeight: 'bold'}}>
+            Smart Tiffin Service
+          </Text>
+          <Text
+            style={{
+              color: 'yellow',
+              fontSize: 19,
+              fontWeight: 'bold',
+              marginBottom: 20,
+            }}>
+            Let's Login
+          </Text>
+          <Field
+            placeholder="Email / Username"
+            keyboardType={'email-address'}
+          />
+          <Field placeholder="Password" secureTextEntry={true} />
+          <View
+            style={{alignItems: 'flex-end', width: '78%', paddingRight: 16, marginBottom: 200}}>
+            <Text style={{color:  'brown', fontWeight: 'bold', fontSize: 16}}>
+              Forgot Password ?
+            </Text>
+          </View>
+          <Btn textColor='white' bgColor={ 'blue'} btnLabel="Login" Press={() => alert("Logged In")} />
+          <View style={{ display: 'flex', flexDirection :'row', justifyContent: "center" }}>
+            <Text style={{ fontSize: 16, fontWeight:"bold" }}>Don't have an account ? </Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate("Signup")}>
+            <Text style={{ color: darkGreen, fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Background>
+  );
+};
+
+export default Login;
